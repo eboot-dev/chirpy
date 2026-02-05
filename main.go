ae4b6b38532b3d21cb1eb3dc5d94426baa967ec9
E <email@example.com>
2026-02-05 23:48:38 +0100

    add assets route to serve logo.png and use log.Fatal for error handling

--- main.go
+++ main.go
@@ -3,19 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
-	// "errors"
+	"log"
 )
 
 func main() {
 	fmt.Println("Starting up...")
 	mux := http.NewServeMux()
 	mux.Handle("/",http.FileServer(http.Dir(".")))
-	server := http.Server{
+	mux.Handle("/assets",http.FileServer(http.Dir("./assets")))
+	server := &http.Server{
 		Handler: mux,
 		Addr: ":8080",
 	}
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Printf("ERROR: can't start server, %w",err)
-	}
+	log.Fatal(server.ListenAndServe())
 }
