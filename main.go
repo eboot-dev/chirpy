bce37d661b47052277d117fd871b2bb8bd2d94d2
E <email@example.com>
2026-02-07 10:57:35 +0100

    Migrate to /admin endpoints. Serve JSON replies and handle JSON requests. Add message validation

--- main.go
+++ main.go
@@ -5,11 +5,61 @@ import (
 	"net/http"
 	"log"
 	"sync/atomic"
+	"encoding/json"
 )
 
 
-/* Handlers */
+/* Utils */
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type errorStruct struct {
+		Error string `json:"error"`
+	}
+	err := errorStruct { Error: msg }
+	respondWithJSON(w,code,err)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+/* Handlers */
+func validationHandler(w http.ResponseWriter, req *http.Request) {
+	type chirp struct {
+        Content string `json:"body"`
+    }
+
+    decoder := json.NewDecoder(req.Body)
+    msg := chirp{}
+    err := decoder.Decode(&msg)
+    if err != nil {
+		log.Printf("Error decoding chirp body: %s", err)
+		respondWithError(w,http.StatusBadRequest,"Error decoding chirp body")
+		return
+    }
+
+	if len(msg.Content) > 140 {
+		respondWithError(w,http.StatusBadRequest,"Chirp is too long")
+		return
+	}
+
+	// Response
+	type respStruct struct {
+        Valid bool `json:"valid"`
+    }
+    response := respStruct{
+        Valid: true,
+    }
+	respondWithJSON(w, http.StatusOK, response)
+}
 /* Middlewares */
 
 // This is a looging middleware. It logs the Method and URL.Path of a request and pass it to the next handler to be processed
@@ -30,10 +80,11 @@ func main() {
 	const assetsRoutePath = "/assets"
 	const assetsFilePath = "/assets"
 
-	const readinessRoutePath = "/healthz"
-	const metricsRoutePath = "/metrics"
-	const metricsResetRoutePath = "/reset"
+	const readinessRoutePath = "GET /api/healthz"
+	const metricsRoutePath = "GET /admin/metrics"
+	const metricsResetRoutePath = "POST /admin/reset"
 
+	const validationRoutePath = "POST /api/validate_chirp"
 	// init apiConfig struct
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
@@ -59,6 +110,8 @@ func main() {
 	// Metrics Reset Endpoint
 	mux.HandleFunc(metricsResetRoutePath,apiCfg.metricsResetHandler)
 	
+	// Validation Chirp Endpoint
+	mux.HandleFunc(validationRoutePath,validationHandler)
 	server := &http.Server{
 		Handler: mux,
 		Addr: ":" +port,
--- metrics.go
+++ metrics.go
@@ -13,9 +13,9 @@ type apiConfig struct {
 
 // Metrics endpoint handler. It shows the number of requests made to the server.
 func (c *apiConfig) metricsHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type","text/plain; charset=utf-8")
+	w.Header().Set("Content-Type","text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", c.fileserverHits.Load())))
+	w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", c.fileserverHits.Load())))
 }
 
 // Reset Metrics endpoint handler. It resets the counter of number of requests made to the server.
